ba83568c73b4f56a78b82cf5506657bb62148007
TheDarkRoot <37486414+TheDarkRoot@users.noreply.github.com>
2020-07-21 14:59:01 +0300

    Add files via upload

---
+++ Terpack.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+pkg install git -y
+pkg install python -y
+pkg install python2 -y
+pkg install python3 -y
+pkg install cowsay -y
+pkg install wget -y
+pkg install curl -y
+pkg install ruby -y
+pkg install php -y
+pkg install toilet -y
+pkg install vim -y
+pkg install nano
+pkg install unzip
+pkg install figlet
+pkg install neofetch
+pkg install nano
+pkg install cmatrix
+pkg install perl
+pkg install cat
+pip install --upgrade pip
+pip install pillow
+gem install lolcat
+apt update
+apt upgrade -y
+cd /data/data/com.termux/files/home
+rm -rf TDR-X
+rm -rf Hack-X
+mkdir TDR-X
+chmod +x Terpack.sh
+cd /data/data/com.termux/files/home/TDR-X
+curl https://raw.githubusercontent.com/TheDarkRoot/Terkey/master/Terkey.py -o Terkey.py
+curl https://raw.githubusercontent.com/TheDarkRoot/Termup/master/Termup.sh -o Termup.sh
+curl https://raw.githubusercontent.com/TheDarkRoot/Terpack/master/Terpack.sh -o Terpack.sh
+curl https://raw.githubusercontent.com/TheDarkRoot/Terpack/master/TheDarkRoot.py -o TheDarkRoot.py
+curl https://raw.githubusercontent.com/TheDarkRoot/Hasher/master/Hasher.py -o Hasher.py
+curl https://raw.githubusercontent.com/TheDarkRoot/Hashgen/master/Hashgen.py -o Hashgen.py
+curl https://raw.githubusercontent.com/TheDarkRoot/UserID/master/UserID.sh -o UserID.sh
+curl https://raw.githubusercontent.com/TheDarkRoot/Hasher/master/Wordlist.txt -o Wordlist.txt
+curl https://raw.githubusercontent.com/TheDarkRoot/Fbinfos/master/Fbinfos.py -o Fbinfos.py
+curl https://raw.githubusercontent.com/TheDarkRoot/Terpack/master/bash.bashrc -o bash.bashrc
+chmod +x *
+python TheDarkRoot.py
