a6c3c5acdf067bc308e2a739afd4e655e529681d
TheDarkRoot <37486414+TheDarkRoot@users.noreply.github.com>
2020-11-05 02:45:19 +0300

    Update Terpack.sh

--- Terpack.sh
+++ Terpack.sh
@@ -14,53 +14,16 @@ clear
 # EOF
 cowsay -f eyes The quieter you become, the more you are able to hear. | lolcat
 toilet -f big ' TheDarkRoot' -F gay | lolcat" > /data/data/com.termux/files/usr/etc/bash.bashrc
-rm -rf Tdr-Tool;rm -rf Hack-Tool;mkdir Tdr-Tool;mkdir Hack-Tool
-chmod +x Terpack.sh
-cd /data/data/com.termux/files/home/Tdr-Tool
-curl https://raw.githubusercontent.com/TheDarkRoot/Terkey/master/Terkey.py -o Terkey.py
-curl https://raw.githubusercontent.com/TheDarkRoot/Termup/master/Termup.sh -o Termup.sh
-curl https://raw.githubusercontent.com/TheDarkRoot/Terpack/master/Terpack.sh -o Terpack.sh
-curl https://raw.githubusercontent.com/TheDarkRoot/Terpack/master/Tdr-Tool.py -o Tdr-Tool.py
-curl https://raw.githubusercontent.com/TheDarkRoot/Hasher/master/Hasher.py -o Hasher.py
-curl https://raw.githubusercontent.com/TheDarkRoot/Hashgen/master/Hashgen.py -o Hashgen.py
-curl https://raw.githubusercontent.com/TheDarkRoot/UserID/master/UserID.sh -o UserID.sh
-curl https://raw.githubusercontent.com/TheDarkRoot/Hasher/master/Wordlist.txt -o Wordlist.txt
-curl https://raw.githubusercontent.com/TheDarkRoot/Fbinfos/master/Fbinfos.py -o Fbinfos.py
-curl https://raw.githubusercontent.com/TheDarkRoot/AnonSMS/master/AnonSMS.py -o AnonSMS.py
-curl https://raw.githubusercontent.com/TheDarkRoot/Terpack/master/bash.bashrc -o bash.bashrc
-chmod +x *
-cd /data/data/com.termux/files/home/Hack-Tool
-git clone https://github.com/CiKu370/Osif.git
-cd Osif
-chmod +x *
-pip2 install -r requirements.txt
-cd /data/data/com.termux/files/home/Hack-Tool
-git clone https://github.com/lulz3xploit/LittleBrother.git
-cd LittleBrother
-chmod +x *
-cd /data/data/com.termux/files/home/Hack-Tool
-git clone https://github.com/TheSpeedX/TBomb.git
-cd TBomb
-chmod +x *
-cd /data/data/com.termux/files/home/Hack-Tool
-git clone https://github.com/thewhiteh4t/Seeker.git
-chmod +x *
-cd /data/data/com.termux/files/home/Hack-Tool
-git clone https://github.com/xHak9x/SocialPhish.git
-cd SocialPhish
-chmod +x *
-cd /data/data/com.termux/files/home/Hack-Tool
-git clone https://github.com/lightos/Credmap.git
-cd Credmap
-chmod +x *
-cd /data/data/com.termux/files/home/Hack-Tool
-git clone https://github.com/rajkumardusad/IP-Tracer.git
-cd IP-Tracer
-chmod +x *
-cd /data/data/com.termux/files/home/Hack-Tool
-git clone https://github.com/sandiwijayani1/Spamwa.git
-cd Spamwa
-chmod +x *
-pip install requests
-cd /data/data/com.termux/files/home/Tdr-Tool
-python Tdr-Tool.py
+rm -rf Tdr-Tool;rm -rf Hack-Tool;mkdir Tdr-Tool;mkdir Hack-Tool;mkdir .termux;nano termux.properties
+echo "extra-keys = [['ESC','/','-','HOME','UP','END','PGUP'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN']]" > /data/data/com.termux/files/home/.termux/termux.properties
+termux-reload-settings
+cd /data/data/com.termux/files/home/Tdr-Tool/
+mkdir Hasher;mkdir Hashgen;mkdir UserID;mkdir AnonSMS
+curl https://raw.githubusercontent.com/TheDarkRoot/Terpack/master/Terpack.sh -o Terpack.sh;chmod +x *.sh
+cd /data/data/com.termux/files/home/Tdr-Tool/Hasher;curl https://raw.githubusercontent.com/TheDarkRoot/Hasher/master/Hasher.py -o Hasher.py;chmod +x *
+cd /data/data/com.termux/files/home/Tdr-Tool/Hashgen;curl https://raw.githubusercontent.com/TheDarkRoot/Hashgen/master/Hashgen.py -o Hashgen.py;chmod +x *
+cd /data/data/com.termux/files/home/Tdr-Tool/UserID;curl https://raw.githubusercontent.com/TheDarkRoot/UserID/master/UserID.sh -o UserID.sh;chmod +x *
+cd /data/data/com.termux/files/home/Tdr-Tool/Hasher;curl https://raw.githubusercontent.com/TheDarkRoot/Hasher/master/Wordlist.txt -o Wordlist.txt
+cd /data/data/com.termux/files/home/Tdr-Tool/AnonSMS;curl https://raw.githubusercontent.com/TheDarkRoot/AnonSMS/master/AnonSMS.py -o AnonSMS.py;chmod +x *
+cd /data/data/com.termux/files/home/Tdr-Tool/
+cp Terpack.sh /data/data/com.termux/files/home/
