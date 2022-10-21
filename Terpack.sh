41fbf52854463f5e8cd8b663dd18cdda5bb39ece
TheDarkRoot <37486414+TheDarkRoot@users.noreply.github.com>
2022-10-22 00:51:43 +0300

    Terpack
    TheDarkRoot termux package installer.

--- Terpack.sh
+++ Terpack.sh
@@ -32,7 +32,7 @@ W="\033[0;1m"   # White
 R="\033[31;1m"  # Red
 C="\033[36;1m"  # Cyan
 M="\033[35;1m"  # Magenta
-clear;echo -e "$CC\n [$YY↓$CC]$GG Updating...\n";cd ~/;apt upgrade -y;apt update;pkg upgrade -y;pkg update;chmod +x *;clear;
+clear;echo -e "$CC\n [$YY↓$CC]$GG Updating...\n";apt update;apt upgrade -y;cd ~/;rm -rf Terpack.sh;curl https://raw.githubusercontent.com/TheDarkRoot/Terpack/master/Terpack.sh -o Terpack.sh;chmod +x *;clear;
 #Terpack Banner
 echo -e "\n$CC #######$YY ##################$CC ######$YY #####################
 $CC    #    ###### #####       #     #  ####   ####  #    #
@@ -55,7 +55,7 @@ $GG 0{===================================================}0\n"
 echo -e "$CC [$YY*$CC]$GG Termux setup storage...";
 ( rm -rf storage;termux-setup-storage; ) &> /dev/null & spin;
 echo -e "$CC [$YY*$CC]$GG Pkg installing...";
-( pkg install git -y;pkg install python -y;pkg install python2;pkg install toilet -y;pkg install ruby -y;pkg install php -y;pkg install cowsay -y;pkg install figlet;pkg install wget -y;pkg install curl -y;pkg install vim -y;pkg install proot;pkg install crunch;pkg install neofetch;pkg install nano;pkg install cmatrix;pkg install openssh -y;apt install openssl-tool -y;pkg install zsh;pkg install sl;pkg install tmate -y;apt install bash -y;pkg install termux-api; ) &> /dev/null & spin;
+( pkg install toilet -y;pkg install git -y;pkg install python -y;pkg install python2;pkg install ruby -y;pkg install php -y;pkg install cowsay -y;pkg install figlet;pkg install wget -y;pkg install curl -y;pkg install vim -y;pkg install proot;pkg install crunch;pkg install neofetch;pkg install nano;pkg install cmatrix;pkg install openssh -y;apt install openssl-tool -y;pkg install zsh;pkg install sl;pkg install tmate -y;apt install bash -y;pkg install termux-api; ) &> /dev/null & spin;
 echo -e "$CC [$YY*$CC]$GG Pip installing...";
 ( gem install lolcat;pip install --upgrade pip;pip2 install --upgrade pip;pip install bs4;pip2 install bs4;pip install requests;pip2 install requests;pip install mechanize;pip2 install mechanize;pip2 install passlib;pip2 install progressbar;pip install pillow;python -m pip install requests; ) &> /dev/null & spin;
 echo -e "$CC [$YY*$CC]$GG Tor installing...";
